b683ae033c4975a19899012220540f3dba03eb57
Pusunuri Srikanth <srikanthpusunuri1234@gmail.com>
2025-04-06 07:43:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 3
-ami = ""
+ami = "ami-0a9a48ce4458e384e"
 key_name = "sri11"
-vpc_security_group_ids = ""
+vpc_security_group_ids = "sg-0e6403a34209c36b0"
 instance_type ="t2.micro"
 tags = {
 Name = var.instace_name[count.index]
